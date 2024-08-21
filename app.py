db4ffc01a5a3dcfdb0624720b1b3777459f9c47a
Cristian <cristiankhan23@gmail.com>
2024-08-21 10:45:02 -0300

    Add endpoints and necessary imports

--- app.py
+++ app.py
@@ -1,3 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Text, Optional
+from datetime import datetime
+from uuid import uuid4 as uuid
 
 app = FastAPI()
+
+posts = []
+
+# Post Model
+class Post(BaseModel):
+    id: Optional[str] 
+    title: str
+    author: str
+    content: Text
+    created_at: datetime = datetime.now()
+    published_at: Optional[datetime]
+    published: bool = False
+
+@app.get('/')
+def read_root():
+    return {"Wellcome": "Wellcome to my REST API"}
+
+@app.get('/posts')
+def get_posts():
+    return posts
+
+@app.post('/posts')
+def save_posts(post: Post):
+    post.id = str(uuid())
+    posts.append(post)
+    return posts[-1]
+
+@app.get('/posts/{post_id}')
+def get_post(post_id: str):
+    for post in posts:
+        if post.id == post_id:
+            return post
+    raise HTTPException(status_code=404, detail="Post Not found")
